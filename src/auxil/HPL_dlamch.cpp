74c9fe6903d21b2d39eb2eb2edc8264e6e3ad49b
Piotr Luszczek <luszczek@icl.utk.edu>
2021-09-12 17:39:40 -0400

    Change variable name clashing with C++ keyword

--- src/auxil/HPL_dlamch.cpp
+++ src/auxil/HPL_dlamch.cpp
@@ -746,7 +746,7 @@ static void HPL_dlamc5( BETA, P, EMIN, IEEE, EMAX, RMAX )
  * .. Local Variables ..
  */
    double                     oldy=HPL_rzero, recbas, y, z;
-   int                        exbits=1, expsum, i, lexp=1, nbits, try,
+   int                        exbits=1, expsum, i, lexp=1, nbits, try0,
                               uexp;
 /* ..
  * .. Executable Statements ..
@@ -758,10 +758,10 @@ static void HPL_dlamc5( BETA, P, EMIN, IEEE, EMAX, RMAX )
  * required number RMAX).
  */
 l_10:
-   try = (int)( (unsigned int)(lexp) << 1 );
-   if( try <= ( -EMIN ) ) { lexp = try; exbits++; goto l_10; }
+   try0 = (int)( (unsigned int)(lexp) << 1 );
+   if( try0 <= ( -EMIN ) ) { lexp = try0; exbits++; goto l_10; }
 
-   if( lexp == -EMIN ) { uexp = lexp; } else { uexp = try; exbits++; }
+   if( lexp == -EMIN ) { uexp = lexp; } else { uexp = try0; exbits++; }
 /*
  * Now -lexp is less than or equal to EMIN, and -uexp is greater than or
  * equal to EMIN. exbits is the number of bits needed to store the expo-
