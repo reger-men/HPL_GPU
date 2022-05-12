eeff79f9b495c79a41435edaf2955047ada7a34e
Vicky Tsang <vicky.w.tsang@gmail.com>
2022-05-12 23:27:58 +0200

    add readme for rocm. clean up run script

--- scripts/runHPL_singleNode.sh
+++ scripts/runHPL_singleNode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MPI_DIR=$HOME/ompi/bin
+MPI_DIR=$MPI_HOME/bin
 num_cpu_cores=16
 num_process=1
 
@@ -9,5 +9,5 @@ export OMP_NUM_THREADS=${num_cpu_cores}
 export LD_LIBRARY_PATH=openblas:$LD_LIBRARY_PATH
 
 # ./xhpl
-HSA_ENABLE_SDMA=1 ${MPI_DIR}/mpirun  -mca btl '^openib' --mca pml ucx -x UCX_RNDV_PIPELINE_SEND_THRESH=256k -x UCX_RNDV_FRAG_SIZE=rocm:4m --allow-run-as-root -np ${num_process} --map-by node:PE=${num_cpu_cores} --bind-to core:overload-allowed --report-bindings ./xhplhip
+HSA_ENABLE_SDMA=1 ${MPI_DIR}/mpirun --allow-run-as-root -np ${num_process} --map-by node:PE=${num_cpu_cores} --bind-to core:overload-allowed --report-bindings ./xhplhip
 grep --color "e+" HPL.out
